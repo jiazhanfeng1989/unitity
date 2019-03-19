93ab70d3a3eb5464401ed9ee4046a574e24b866f
jiazhanfeng <jiazhanfeng1989@126.com>
2019-03-19 22:08:51 +0800

    add common function

---
+++ checkfile.cpp
@@ -0,0 +1,31 @@
+#include <string>
+#include <sys/stat.h>
+
+enum class enFILESTATUS
+{
+    DIRECTORY,
+    FILE_NOT_EMPTY,
+    FILE_EMPTY,
+    NOT_EXIST,
+    FILE_EXIST
+};
+
+enFILESTATUS checkfile(const std::string& theFileName)
+{
+    struct stat statinfo;
+    if (stat(theFileName.c_str(), &statinfo) == 0)
+    {
+        if ((statinfo.st_mode & S_IFMT) == S_IFDIR)
+            return enFILESTATUS::DIRECTORY;
+
+        if (statinfo.st_size == 0)
+            return enFILESTATUS::FILE_EMPTY;
+    }
+    else
+    {
+        if (errno == ENOENT)
+            return enFILESTATUS::NOT_EXIST;
+    }
+
+    return enFILESTATUS::FILE_NOT_EMPTY;
+}
---
+++ getpasswd.cpp
@@ -0,0 +1,77 @@
+#include <termio.h>
+#include <iostream>
+
+class CPassword
+{
+    
+public:
+    CPassword();
+    ~CPassword();
+    
+    void IputPassword();
+    std::string GetPassword();
+    
+private:
+    int sh_getch();
+    
+private:
+    std::string mypsw;
+};
+
+CPassword::CPassword()
+{
+    mypsw="";
+}
+
+CPassword::~CPassword()
+{
+    
+}
+
+int CPassword::sh_getch(void)
+{
+    int cr;
+    struct termios nts, ots;
+    if (tcgetattr(0, &ots) < 0)
+    {
+       return EOF;
+    }
+
+    nts = ots;
+    cfmakeraw(&nts);
+    if (tcsetattr(0, TCSANOW, &nts) < 0)
+    {
+        return EOF;
+    }
+
+    cr = getchar();
+    if (tcsetattr(0, TCSANOW, &ots) < 0)
+    {
+        return EOF;
+    }
+
+    return cr;
+}
+
+void CPassword::IputPassword()
+{
+   std::cout<<"Enter Passwd:";
+   int temp_c = -1;
+   
+   while(true)
+   {
+       temp_c=sh_getch();
+       if(temp_c==13 || temp_c==3 || temp_c==4 ||temp_c==27)
+           break;
+       mypsw+=temp_c;
+       std::cout<<"*";
+   }
+   std::cout<<std::endl;
+}
+
+std::string CPassword::GetPassword()
+{
+   return mypsw;
+}
+
+
