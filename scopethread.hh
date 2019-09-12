a2d62ccb3833a568aaba54147af2e0c51b8f821a
jiazhanfeng <jiazhanfeng1989@126.com>
2019-09-12 11:24:16 +0800

    Update scopethread.hh

--- scopethread.hh
+++ scopethread.hh
@@ -18,4 +18,38 @@ public:
     scoped_thread(scoped_thread const&)=delete;
     scoped_thread& operator=(scoped_thread const&)=delete;
 };
+
+class ScopeGuard
+{
+public:
+    template<typename T>
+    explicit ScopeGuard(T&& f)
+        : f_(std::forward<T>(f))
+    {
+        assert(f_);
+    }
+    
+    /**
+     * @brief Destructor of scope guard, will call injected function.
+     */
+    virtual ~ScopeGuard()
+    {
+        if (f_)
+        {
+            f_();
+        }
+    }
+public:
+    /**
+     * @brief Cancel function call when exit current scope.
+     * @param N/A
+     * @return N/A
+     */
+    void dismiss()
+    {
+        f_ = {};
+    }
+private:
+    std::function<void ()> f_;
+};  //  class ScopeGuard
 #endif
